91660bfbf9ef1e2e711ac8e5095a46f3ce1e4865
ThomFDV <t.ferreira@monsieurbiz.com>
2019-04-26 10:55:11 +0200

    Add admin routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./core/component/home/home.component";
+import {AdminHomeComponent} from "./admin/component/admin-home/admin-home.component";
+import {NotFoundComponentComponent} from "./core/component/not-found-component/not-found-component.component";
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'admin',
+    component: AdminHomeComponent
+  },
+  {
+    path: 'nawak',
+    component: NotFoundComponentComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'nawak'
+  }
 ];
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './admin/component/footer/footer.component';
+import { HeaderComponent } from './admin/component/header/header.component';
+import { AdminHomeComponent } from './admin/component/admin-home/admin-home.component';
+import { HomeComponent } from './core/component/home/home.component';
+import { NotFoundComponentComponent } from './core/component/not-found-component/not-found-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    AdminHomeComponent,
+    HomeComponent,
+    NotFoundComponentComponent
   ],
   imports: [
     BrowserModule,
