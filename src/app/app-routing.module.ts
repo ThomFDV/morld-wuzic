e664af88fc04b6dc82b13eef625acebb0303be30
ThomFDV <t.ferreira@monsieurbiz.com>
2019-05-31 12:47:52 +0200

    Add login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from "./core/components/home/home.component";
 import {NotFoundComponentComponent} from "./core/components/not-found-component/not-found-component.component";
 import {AdminRoutingModule} from "./admin/admin-routing.module";
+import {LoginComponent} from "./core/components/login/login.component";
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     pathMatch: 'full'
+  },{
+    path: 'login',
+    component: LoginComponent
   },
   {
     path: 'nawak',
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import {HomeComponent} from "./components/home/home.component";
 import {NotFoundComponentComponent} from "./components/not-found-component/not-found-component.component";
 import {RouterModule} from "@angular/router";
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
-  declarations: [ HomeComponent, NotFoundComponentComponent],
+  declarations: [ HomeComponent, NotFoundComponentComponent, LoginComponent],
   imports: [
     CommonModule,
     RouterModule
