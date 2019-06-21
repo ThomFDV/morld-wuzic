f7767f9323791f7adda820d9c941376b30b0d01e
Elise Allirol <allirol.elise@gmail.com>
2019-06-21 15:46:18 +0200

    add translation

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {HomeComponent} from "./components/home/home.component";
-import {NotFoundComponentComponent} from "./components/not-found-component/not-found-component.component";
-import {RouterModule} from "@angular/router";
+import {HomeComponent} from './components/home/home.component';
+import {NotFoundComponentComponent} from './components/not-found-component/not-found-component.component';
+import {RouterModule} from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {
   MatButtonModule,
@@ -13,14 +13,16 @@ import {
   MatIconModule,
   MatInputModule, MatRadioModule,
   MatStepperModule
-} from "@angular/material";
+} from '@angular/material';
 import { RegisterComponent } from './components/register/register.component';
-import {GameComponent} from "./components/game/game.component";
-import {ScoreComponent} from "./components/score/score.component";
+import {GameComponent} from './components/game/game.component';
+import {ScoreComponent} from './components/score/score.component';
+import {TranslateModule, TranslateService} from '@ngx-translate/core';
 
 @NgModule({
   declarations: [ HomeComponent, NotFoundComponentComponent, LoginComponent, RegisterComponent, GameComponent, ScoreComponent],
   imports: [
+    TranslateModule,
     CommonModule,
     RouterModule,
     ReactiveFormsModule,
@@ -33,6 +35,9 @@ import {ScoreComponent} from "./components/score/score.component";
     MatDividerModule,
     MatStepperModule,
     MatRadioModule
+  ],
+  providers: [
+    TranslateService
   ]
 })
 export class CoreModule { }
